5208789dd1eb4b8bf11b8f274408a6fe1bfdd7b2
Cenk Dolek <cdolek@gmail.com>
2016-01-08 18:19:29 -0500

    language strings

--- graphcommons.php
+++ graphcommons.php
@@ -190,7 +190,7 @@ class GraphCommons {
             'language'      => array(                
                 'hubsearchtext' => __('Select if you would like to search in hubs','graphcommons'),
                 'searchkeyword' => __('Search keyword', 'graphcommons'),
-                'typesomething' => __('Type something, results will be displayed here.'),
+                'typesomething' => __('Type something, results will be displayed here'),
                 'addgcnodecard' => __('Insert Graph Commons Node Card', 'graphcommons'),
                 'noresultsfound' => __('No results found for:', 'graphcommons')
             )
