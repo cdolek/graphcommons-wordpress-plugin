065adae0649021d67c7ff8f6832bcb6fb31c1127
Cenk Dolek <cdolek@gmail.com>
2016-01-08 16:54:41 -0500

    cleanup

--- graphcommons.php
+++ graphcommons.php
@@ -42,26 +42,28 @@ class GraphCommons {
         // Set up l10n        
         load_plugin_textdomain( 'graphcommons', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );                
 
-        // actions */
-        add_action( 'pre_get_posts', array(&$this, 'init') );
-        add_action( 'admin_menu', array(&$this, 'gc_create_admin_menu' ) );
-        add_action( 'admin_init', array(&$this, 'gc_admin_init' ) );
-        add_action( 'admin_notices', array(&$this, 'gc_admin_notice' ) );
-        add_action( 'init',  array(&$this, 'gc_custom_rewrite_tag'), 10, 0 );
-        add_action( 'wp_ajax_get_nodes_json', array(&$this, 'get_nodes_json' ) );
-
-        add_action( 'admin_footer', array(&$this, 'gc_link_dialog' ) ) ;
+        // actions
+        add_action( 'pre_get_posts',            array(&$this, 'init') );
+        add_action( 'admin_menu',               array(&$this, 'gc_create_admin_menu' ) );
+        add_action( 'admin_init',               array(&$this, 'gc_admin_init' ) );
+        add_action( 'admin_notices',            array(&$this, 'gc_admin_notice' ) );
+        add_action( 'init',                     array(&$this, 'gc_custom_rewrite_tag'), 10, 0 );    
+        add_action( 'wp_ajax_get_nodes_json',   array(&$this, 'get_nodes_json' ) );
+        add_action( 'wp_ajax_get_hubs_json',    array(&$this, 'get_hubs_json' ) );
+        add_action( 'admin_footer',             array(&$this, 'gc_admin_footer' ) ) ;        
+        add_action( 'media_buttons',            array( &$this, 'gc_media_buttons' ), 11 );
         
-        add_action( 'media_buttons', array( &$this, 'media_buttons' ), 11 );
-
+        /** 
+        todo:
+            add_action( 'init', array( &$this, 'gc_oembed_provider' ) );
+        **/
 
         // shortcodes
-        add_shortcode('graphcommons', array(&$this, 'graphcommons_shortcode') );
+        add_shortcode('graphcommons', array(&$this, 'gc_shortcode') );
 
         // filters
-        add_filter( 'mce_external_plugins', array( &$this, 'graphcommons_add_button' ) );
-        add_filter( 'mce_buttons', array( &$this, 'graphcommons_register_button' ) );        
-    
+        add_filter( 'mce_external_plugins', array( &$this, 'gc_tinymce_plugin' ) );
+        add_filter( 'mce_buttons', array( &$this, 'gc_register_button' ) );            
         add_filter( 'mce_css', array(&$this, 'gc_mce_css' ) );
 
 
@@ -81,31 +83,16 @@ class GraphCommons {
                 *
                 *   GC API Endpoints
 
-                */                
+                */              
 
                 case 'nodes_search':
                     $this->gc_api_nodes_search('https://graphcommons.com/api/v1/nodes/search?query='. $this->keyword . '&limit=' . $this->api_limit);
                     break;
 
-                /**
-                *
-                *    DEBUG
-                                
-                */                
-               
-                case 'debug':  
-                    // http://yoursite/graphcommons_api/action/keyword/
-                    var_dump( $this->action );
-                    var_dump( $this->keyword );                    
-
+                case 'hubs':
+                    $this->get_hubs_json();
                     break;
 
-                /**                
-                *
-                *    DEFAULT
-                                
-                */
-                
                 default:                
                     break;
             } // switch
@@ -116,12 +103,25 @@ class GraphCommons {
 
     }
 
+    // get json for nodes
     function get_nodes_json() {
-
         $keyword = $_POST['keyword'];
+        $hub = $_POST['hub'];
+        $url = 'https://graphcommons.com/api/v1/nodes/search?query='. urlencode($keyword) . '&limit=' . $this->api_limit;       
+        if ( $hub !== '' ) {
+            $url = $url . '&hub=' . $hub;
+        }
 
         header( 'Content-type: application/json' );
-        $url = 'https://graphcommons.com/api/v1/nodes/search?query='. $keyword . '&limit=' . $this->api_limit;        
+        echo $this->get_url($url);
+        die();
+    }
+
+    // getting hubs list
+    function get_hubs_json() {
+        $url = 'https://graphcommons.com/hubs.json';        
+
+        header( 'Content-type: application/json' );
         echo $this->get_url($url);
         die();
     }
@@ -130,10 +130,17 @@ class GraphCommons {
     function gc_api_nodes_search( $url ) {
         header( 'Content-type: application/json' );
         echo $this->get_url($url);
+        die();
+    }
+
+    // Register oEmbed providers
+    function gc_oembed_provider() {
+        wp_oembed_add_provider( 'https://graphcommons.com/nodes/*', 'https://graphcommons.com/oembed', false );
+        wp_oembed_add_provider( 'https://graphcommons.com/graphs/*', 'https://graphcommons.com/oembed', false );
     }
 
     // graphcommons shortcode
-    function graphcommons_shortcode( $atts ) {
+    function gc_shortcode( $atts ) {
 
         $atts = shortcode_atts( array(
             'id'    => 'id',
@@ -147,7 +154,7 @@ class GraphCommons {
                 $html = '<iframe src="https://graphcommons.com/nodes/'.$atts["id"].'/embed?p=&et=i%C5%9F%20orta%C4%9F%C4%B1d%C4%B1r&g=true" frameborder="0" style="overflow:auto;width:320px;min-width:320px;height:100%;min-height:460px;border:1px solid #CCCCCC;" width="320" height="460"></iframe>';
                 break;            
             default:
-                $html = '<strong>Error:</strong> Unknown embed type - Graph Commons Wordpress Plugin';
+                $html = __('<strong>Error:</strong> Unknown embed type - Graph Commons Wordpress Plugin', 'graphcommons');
                 break;
         }
 
@@ -178,7 +185,13 @@ class GraphCommons {
         wp_register_script( 'knockoutjs', plugins_url( '/js/knockout.min.js', __FILE__ ), array('jquery'), '3.4.0', true );
         wp_register_script( 'gc-script', plugins_url( '/js/graphcommons.js', __FILE__ ), array('jquery'), '1.0.0', true );
 
-        wp_localize_script( 'gc-script', 'graphcommons', array('api_key' => $this->api_key, 'plugin_url' => $this->plugin_url ) );
+        wp_localize_script( 'gc-script', 'graphcommons', array(
+            'api_key'       => $this->api_key, 
+            'plugin_url'    => $this->plugin_url,
+            'language'      => array(
+                'hubsearchtext' => __('Select if you would like to search in hubs','graphcommons')
+            )
+        ) );
         
         wp_enqueue_script( array( 'knockoutjs', 'gc-script' ) );
 
@@ -217,7 +230,7 @@ class GraphCommons {
     // show admin notice if api key is missing
     function gc_admin_notice() {
 
-        $api_key = esc_attr( get_option( 'gc-api_key' ) );
+        $api_key = esc_attr( get_option( 'gc-api_key' ) );        
 
         if ( ! empty($api_key) ) {
             return;
@@ -267,8 +280,8 @@ class GraphCommons {
         return $result;
     }
 
-    function media_buttons() {
-        echo '<a href="#" id="insert-graphcommons-node" class="button"><span class="dashicons dashicons-edit" style="margin-top: 3px;"></span> Insert Graph Commons Node Card</a>';
+    function gc_media_buttons() {
+        echo '<a href="#" id="insert-graphcommons-node" class="button"><span class="dashicons dashicons-edit" style="margin-top: 3px;"></span> '.__('Insert Graph Commons Node Card', 'graphcommons').'</a>';
     }
 
     // custom editor style
@@ -276,23 +289,23 @@ class GraphCommons {
         if ( ! empty( $mce_css ) )
             $mce_css .= ',';
 
-        $mce_css .= plugins_url( '/css/graphcommons_tinymce_style.css', __FILE__ );
+        $mce_css .= plugins_url( '/css/gc_tinymce_style.css', __FILE__ );
 
         return $mce_css;
     }
 
-    function graphcommons_add_button( $plugin_array ) {
-        $plugin_array['graphcommons'] = plugins_url( '/js/tinymce-plugin.js',__FILE__ );
+    function gc_tinymce_plugin( $plugin_array ) {
+        $plugin_array['graphcommons'] = plugins_url( '/js/graphcommons-tinymce-plugin.js',__FILE__ );
         return $plugin_array;
     }
     
-    function graphcommons_register_button( $buttons ) {
+    function gc_register_button( $buttons ) {
         array_push( $buttons, 'seperator', 'graphcommons' );
         return $buttons;
     }
 
     function gc_add_rewrite_rules() {
-        add_rewrite_rule('^graphcommons_api/([^/]*)/([^/]*)/?','index.php?gc_action=$matches[1]&gc_keyword=$matches[2]','top');        
+        add_rewrite_rule('^gc_api/([^/]*)/([^/]*)/?','index.php?gc_action=$matches[1]&gc_keyword=$matches[2]','top');        
     }
 
     function gc_custom_rewrite_tag() {
@@ -310,19 +323,28 @@ class GraphCommons {
         flush_rewrite_rules();
     }
 
-    function gc_link_dialog() {
-        $search_panel_visible = '1';
-        // display: none is required here, see #WP27605
-        ?>
-        <div id="graphcommons_debug">
-            <pre data-bind="text: ko.toJSON($data, null, 2)"></pre>
-        </div>
-        <?php
+    function gc_admin_footer() {
+    ?>
+
+        <script type="text/html" id="tmpl-editor-graphcommons">
+            <div class="graphcommons_{{ data.type }}"></div>
+            <div class="gc_node" id="gc_node_{{ data.id }}">
+                <span class="title">{{ data.name }}</span>
+                <span class="type">{{ data.type }}</span>
+                <# if ( data.link ) { #>
+                    <# if ( data.linkhref ) { #>
+                        <a href="{{ data.linkhref }}" class="link dtbaker_button_light">{{ data.link }}</a>
+                    <# } #>
+                <# } #>
+            </div>
+        </script>
+
+    <?php
     }
 
 
-} // class end
+} // class ends
 
-new GraphCommons();
+$graphcommons = new GraphCommons();
 
 ?>
